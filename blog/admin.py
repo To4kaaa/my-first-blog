47cddc531961576d9b82f6bdaa0902dc14b9132b
T04ka <stivo007@yandex.ru>
2025-01-21 12:17:22 +0500

    Изменения HTML Сайта

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
 from .models import Post
-
+# Импортируем модель Post которая находится в models
 admin.site.register(Post)
--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,10 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
-
+# models.CharField — так мы определяем текстовое поле с ограничением на количество символов.
+# models.TextField — так определяется поле для неограниченно длинного текста. Выглядит подходящим для содержимого поста, верно?
+# models.DateTimeField — дата и время.
+# models.ForeignKey — ссылка на другую модель.
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
